0104ab0fedafde11e8fa39d50fc46d66b3b38c78
Fajrulll <arisairis3@gmail.com>
2024-02-12 14:50:49 +0700

    p

--- lib/main.dart
+++ lib/main.dart
@@ -50,67 +50,90 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      extendBody: true,
-      backgroundColor: Colors.orange,
-      body: PageView.builder(
-        controller: _pageController,
-        itemCount: _widgetOptions.length,
-        itemBuilder: (context, index) {
-          return _widgetOptions[index];
-        },
-        onPageChanged: (index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-        },
-      ),
-      bottomNavigationBar: CurvedNavigationBar(
-        height: 55,
-        buttonBackgroundColor: Colors.white,
-        color: Colors.blue,
-        backgroundColor: Colors.transparent,
-        items: <Widget>[
-          Image.asset(
-            'assets/images/Setting.png',
-            height: 26,
-            width: 26,
-            color: _selectedIndex == 0 ? Colors.black : Colors.white,
+    return Stack(
+      children: [
+        // Gambar pattern1 di atas pojok kanan
+        Positioned(
+          top: 0,
+          right: 0,
+          child: Image.asset(
+            'assets/images/pattern1.png',
+            width: 100,
+            height: 100,
           ),
-          Image.asset(
-            'assets/images/Document.png',
-            height: 26,
-            width: 26,
-            color: _selectedIndex == 1 ? Colors.black : Colors.white,
+        ),
+        // Gambar pattern2 di bawah pojok kiri
+        Positioned(
+          bottom: 0,
+          left: 0,
+          child: Image.asset(
+            'assets/images/pattern2.png',
+            width: 100,
+            height: 100,
           ),
-          Image.asset(
-            'assets/images/Home.png',
-            height: 26,
-            width: 26,
-            color: _selectedIndex == 2 ? Colors.black : Colors.white,
+        ),
+        Scaffold(
+          extendBody: true,
+          body: PageView.builder(
+            controller: _pageController,
+            itemCount: _widgetOptions.length,
+            itemBuilder: (context, index) {
+              return _widgetOptions[index];
+            },
+            onPageChanged: (index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
           ),
-          Image.asset(
-            'assets/images/Profile.png',
-            height: 26,
-            width: 26,
-            color: _selectedIndex == 3 ? Colors.black : Colors.white,
+          bottomNavigationBar: CurvedNavigationBar(
+            height: 55,
+            buttonBackgroundColor: Colors.white,
+            color: Colors.blue,
+            backgroundColor: Colors.transparent,
+            items: <Widget>[
+              Image.asset(
+                'assets/images/Setting.png',
+                height: 26,
+                width: 26,
+                color: _selectedIndex == 0 ? Colors.black : Colors.white,
+              ),
+              Image.asset(
+                'assets/images/Document.png',
+                height: 26,
+                width: 26,
+                color: _selectedIndex == 1 ? Colors.black : Colors.white,
+              ),
+              Image.asset(
+                'assets/images/Home.png',
+                height: 26,
+                width: 26,
+                color: _selectedIndex == 2 ? Colors.black : Colors.white,
+              ),
+              Image.asset(
+                'assets/images/Profile.png',
+                height: 26,
+                width: 26,
+                color: _selectedIndex == 3 ? Colors.black : Colors.white,
+              ),
+            ],
+            onTap: (index) {
+              if (index == 0) {
+                // Navigate to setting page without animation
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => Setting()));
+              } else {
+                // Navigate to other pages
+                _pageController.animateToPage(
+                  index,
+                  duration: Duration(milliseconds: 200),
+                  curve: Curves.easeInOut,
+                );
+              }
+            },
           ),
-        ],
-        onTap: (index) {
-          if (index == 0) {
-            // Navigate to setting page without animation
-            Navigator.pushReplacement(
-                context, MaterialPageRoute(builder: (context) => Setting()));
-          } else {
-            // Navigate to other pages
-            _pageController.animateToPage(
-              index,
-              duration: Duration(milliseconds: 200),
-              curve: Curves.easeInOut,
-            );
-          }
-        },
-      ),
+        ),
+      ],
     );
   }
 }
